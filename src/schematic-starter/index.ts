b92128d027db1c2a98905ad140db4ab1996a9730
kevin schuchard <schuchard.kevin@gmail.com>
2018-11-18 15:32:56 -0700

    add component & license schematic, schema

--- src/schematic-starter/index.ts
+++ src/schematic-starter/index.ts
@@ -1,9 +1,42 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  chain,
+  externalSchematic
+} from "@angular-devkit/schematics";
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function schematicStarter(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+const licenseText = (name: string) => `
+/**
+ * @license
+ * Copyright ${name} Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+`;
+
+export function schematicStarter(options: any): Rule {
+  return chain([
+    externalSchematic("@schematics/angular", "component", options),
+    (tree: Tree, _context: SchematicContext) => {
+      tree.getDir("./src/app").visit(filePath => {
+        if (!filePath.endsWith(".ts")) {
+          return;
+        }
+
+        const content = tree.read(filePath);
+        if (!content) {
+          return;
+        }
+
+        // Prevent from writing license to files that already have one.
+        const license = licenseText(options.name);
+        if (content.indexOf(license) == -1) {
+          tree.overwrite(filePath, license + content);
+        }
+      });
+      return tree;
+    }
+  ]);
 }
